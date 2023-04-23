0dc75ffd6aa89dd5227b5fa229a058d58d5a5d2a
kartikjain-sudo <kartikjain42@protonmail.com>
2023-04-24 01:41:11 +0530

    feat: basic template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    WinstonModule.forRoot({
+      levels: winston.config.npm.levels,
+      format: winston.format.combine(
+        winston.format.timestamp({
+          format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.splat(),
+        winston.format.colorize()
+      ),
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(winston.format.timestamp(), nestWinstonModuleUtilities.format.nestLike())
+        })
+      ]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
