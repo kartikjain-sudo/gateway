facc13250423e4bbbfd0cff948d87b8db8251fdf
kartikjain-sudo <kartikjain42@protonmail.com>
2023-04-25 23:33:28 +0530

    fix: minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('NODE_PORT');
   const envApp = configService.get('NODE_ENV');
